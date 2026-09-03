311086e653ecc993a1540d95f67bd86cf7c93944
yagavan srikaran <yagavan94@gmail.com>
2026-09-03 11:51:29 +0200

    hei

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'demo-complete.html'));
-  throw new Error('Something crashed');
-  res.send('test');
+  res.sendFile(path.join(__dirname, 'demo-complete.html'));
+  //throw new Error('Something crashed');
+  //res.send('test');
 });
 
 app.listen(port, () => {
