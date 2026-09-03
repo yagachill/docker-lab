3bc9a3ba697fbced3611f409f0056525a7c196d8
yagavan srikaran <yagavan94@gmail.com>
2026-09-03 11:37:10 +0200

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   //res.sendFile(path.join(__dirname, 'demo-complete.html'));
   //throw new Error('Something crashed');
-  res.send('Dette er min aller nyeste push for devops intervjuet.');
+  res.send('test');
 });
 
 app.listen(port, () => {
