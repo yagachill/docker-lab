bc0603047c5bfa4e5812e8edb4cff0b049149e12
yagavan srikaran <yagavan94@gmail.com>
2026-09-03 11:46:41 +0200

    another test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   //res.sendFile(path.join(__dirname, 'demo-complete.html'));
-  //throw new Error('Something crashed');
+  throw new Error('Something crashed');
   res.send('test');
 });
 
